af8afb773e3a606556d1f27d6e88faac23637dd3
Hazem.Solima.devn <hazem.soliman.dev@gmail.com>
2025-02-24 18:06:03 +0200

    handel images response

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { user_route } from './routes/user.route';
 import './config/database';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("src/uploads"));
 app.use("/api/users", user_route);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
